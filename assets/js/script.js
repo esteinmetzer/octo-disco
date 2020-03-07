a0b045ea4b51d1c1f26911d4784b76aca272f35e
esteinmetzer <esteinmetzeruw@gmail.com>
2020-03-07 11:47:23 -0800

    Adding saving of highscores and start of local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,18 @@
 var currentQuestion = 0;
 var time = quizQuestions.length * 15;
-var timer;
+var timerId;
 
 var question = document.getElementById("quizQuestions");
 var questionChoices = document.getElementById("questionChoices");
 var startBtn = document.getElementById("startButton");
 var timer = document.getElementById("currentTime");
 var submit = document.getElementById("submit");
-var name = document.getElementById("name");
+var nameEl = document.getElementById("name");
 
 function quizStart() {
     var startingScreen = document.getElementById("startingScreen");
 
-    timer = setInterval(clockTick, 1000)
+    timerId = setInterval(clockTick, 1000)
 
     timer.textContent = time;
 
@@ -29,5 +29,79 @@ function questionGet() {
     questionTitle.textContent = currentQuestion.title;
 
     questionChoices.innerHTML = "";
-    currentQuestion.questionChoices
+    currentQuestion.questionChoices.foreach(function (questionChoices, i) {
+        var questionChoiceNode = document.createElement("button");
+        questionChoiceNode.setAttribute("class", "questionChoice");
+        questionChoiceNode.setAttribute("value", questionChoiceNode);
+        questionChoiceNode.textContent = i + 1 + ". " + questionChoices;
+        questionChoiceNode.onclick = clickQuestion;
+        questionChoices.appendChild(questionChoiceNode);
+    })
 }
+
+var responseEl = document.getElementById("response")
+function clickQuestion() {
+    if (this.value !== questions[currentQuestion].answer) {
+        time -= 10;
+
+        if (time < 0) {
+            time = 0;
+        }
+        timer.textContent = time;
+
+        responseEl.textContent("Incorrect!")
+    } else {
+        responseEl.textContent("Correct!")
+    }
+    responseEl.setAttribute("class", "response");
+    setTimeout(function () {
+        responseEl.setAttribute("class", "response hide");
+    }, 1000);
+
+    currentQuestion++;
+    if (currentQuestion === questions.length) {
+        quizEnd();
+    } else {
+        questionGet();
+    }
+}
+
+function quizEnd() {
+    clearInterval(timerId)
+
+    var endingScreen = document.getElementById("endingScreen");
+    endingScreen.removeAttribute("class");
+
+    var finalScore = document.getElementById("finalScore");
+    finalScore.textContent = time;
+
+    question.setAttribute("class", "hide");
+}
+
+function clockTick() {
+    time--;
+    timer.textContent = time;
+
+    if (time <= 0) {
+        quizEnd();
+    }
+}
+
+function saveHighscore() {
+    var name = nameEl.value.trim();
+
+    if (name !== "") {
+        var highscore = JSON.parse(window.localStorage.getItem("highscores")) || [];
+
+        var newScore = {
+            score: time,
+            name: name
+        };
+        highscore.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscore));
+
+        window.location.href = "quiz"
+    }
+}
+
+startBtn.onclick = quizStart;
